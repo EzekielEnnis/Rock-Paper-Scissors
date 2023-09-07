3779779e7a8497766d004c47582a0f69b148c9f4
EzekielEnnis <143122905+EzekielEnnis@users.noreply.github.com>
2023-09-07 13:15:03 +0100

    Finish playRound and fix bug with gameRules
    Add all conditionals for the game. Removed "" on
    gameRules obj causing playerSelect to become undefined.
    Changed getComputerChoice to simplify

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
 const getComputerChoice = () => {
-
-    switch(Math.ceil(Math.random()*3)) {
-        case(1): return "rock";
-        case(2): return "paper";
-        case(3): return "scissors";
-    }
+    return Math.ceil(Math.random()*3)
 }
 
 function playRound(playerSelect, computerSelect){
-    playerSelect = playerSelect.toLowerCase;
+    const gameRules = {
+        rock: 1,
+        paper: 2,
+        scissors: 3
+    }
+    playerSelect = gameRules[playerSelect.toLowerCase()];
+
+    switch(playerSelect){
+        case(1): {
+            if(computerSelect === 2){
+                return "You Lose! Paper beats Rock";
+            }
+            else if(computerSelect === 3){
+                return "You Win! Rock beats Scissors";
+            }
+            else{
+                return "Draw!";
+            }
+        }
+        case(2): {
+            if(computerSelect === 1){
+                return "You Win! Paper beats Rock";
+            }
+            else if(computerSelect === 3){
+                return "You Lose! Scissors beats Paper";
+            }
+            else{
+                return "Draw!";
+            }            
+        }
+        case(3): {
+            if(computerSelect === 2){
+                return "You Win! Scissors beats Paper";
+            }
+            else if(computerSelect === 1){
+                return "You Lose! Rock beats Scissors";
+            }
+            else{
+                return "Draw!";
+            }            
+        }
+    }
 }
+console.log(playRound("rock", 3))
